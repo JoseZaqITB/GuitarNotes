a21eadb704174b4e6bb6ffbdf0f34313a72edb68
yoseyusMainComputer <zaquijobs@gmail.com>
2024-10-29 19:27:36 +0100

    _layout: improve storage songs.json in device

--- app/_layout.js
+++ app/_layout.js
@@ -2,15 +2,35 @@ import { Stack } from "expo-router";
 import { colors } from "../style/defaultStyles";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { CreateDefaultSongList } from "../hooks/songList";
+import isFirstTimeLaunch from "../stores/settingsStorage";
+import { ActivityIndicator } from "react-native";
 
 export default function MainLayout() {
-  // use effects
+  const [loading, setLoading] = useState(true);
   // store song list json file in the phone
   useEffect(() => {
-    CreateDefaultSongList().catch((e) => console.log(e));
-  }, []);
+    if (isFirstTimeLaunch()) {
+      CreateDefaultSongList()
+        .then(() => setLoading(false))
+        .catch((e) => console.log(e));
+      return;
+    } else {
+      setLoading(false);
+    }
+  }, [setLoading]);
+
+  if (loading) {
+    return (
+      <ActivityIndicator
+        size="large"
+        color={colors.light.primary}
+        style={{ flex: 1 }}
+      />
+    );
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar style="auto" backgroundColor={colors.light.primary} />
